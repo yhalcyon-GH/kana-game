1dbba699f07c70dd58726603c29ea3e0a58541e6
yhalcyon-GH <halcyonyuki@gmail.com>
2026-09-21 21:11:29 +0700

    Register cleanup CLI tests

--- server/tests/run-tests.php
+++ server/tests/run-tests.php
@@ -73,6 +73,7 @@ $testFiles = [
     __DIR__ . '/ObservabilityLoggingTest.php' => 'KanaGame\\Paddle\\Tests\\observabilityLoggingTests',
     __DIR__ . '/Ops/AuthReadinessCheckTest.php' => 'KanaGame\\Paddle\\Tests\\authReadinessCheckTests',
     __DIR__ . '/Ops/EphemeralDataCleanupRepositoryTest.php' => 'KanaGame\\Paddle\\Tests\\ephemeralDataCleanupRepositoryTests',
+    __DIR__ . '/Ops/EphemeralDataCleanupCliTest.php' => 'KanaGame\\Paddle\\Tests\\ephemeralDataCleanupCliTests',
     __DIR__ . '/Ops/ReleaseIntegrityCheckTest.php' => 'KanaGame\\Paddle\\Tests\\releaseIntegrityCheckTests',
 ];
 
